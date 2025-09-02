3821e5cf9848e53219b3cc383cb42929de6903f2
Leonnel Zanguim <leonelzanguim@yahoo.fr>
2025-09-02 17:06:34 +0200

    mongoose orm

--- movie/model.js
+++ movie/model.js
@@ -1,66 +1,50 @@
-// mongoModel.js
-import { MongoClient, ObjectId } from 'mongodb';
+import mongoose from 'mongoose';
 
-let collection = null;
+mongoose.connect('mongodb://localhost:27017/moviedb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-async function connect() {
-  if (collection) {
-    return collection;
-  }
-  const client = new MongoClient('mongodb://localhost:27017');
-  await client.connect();
-  const db = client.db('moviedb');
-  collection = db.collection('Movie');
-  return collection;
-}
+const Movie = mongoose.model('Movie', {
+  id: Number,
+  title: String,
+  year: Number,
+});
 
 export async function initDb() {
-  const movies = await connect(); // ✅ récupère la collection
-  if ((await movies.estimatedDocumentCount()) === 0) {
-    await movies.insertMany([
+  const count = await Movie.estimatedDocumentCount();
+  if (count === 0) {
+    await Movie.insertMany([
       { id: 1, title: 'Iron Man', year: 2008 },
       { id: 2, title: 'Thor', year: 2011 },
       { id: 3, title: 'Captain America', year: 2011 },
     ]);
-    console.log('Seed MongoDB ✅');
+    console.log('Seed MongoDB (via Mongoose) ✅');
   }
 }
 
-export async function getAll() {
-  const collection = await connect();
-  const docs = await collection.find({});
-  return docs.toArray();
+export function getAll() {
+  return Movie.find({});
 }
 
-async function insert(movie) {
-  movie.id = Date.now();
-  const collection = await connect();
-  const data = collection.insertOne(movie);
-  return data;
-}
-
-export function save(movie) {
+export async function save(movie) {
   if (!movie.id) {
-    return insert(movie);
+    const newMovie = new Movie(movie);
+    newMovie.id = Date.now();
+    return newMovie.save();
   } else {
-    return update(movie);
+    const existingMovie = await get(parseInt(movie.id, 10));
+    existingMovie.title = movie.title;
+    existingMovie.year = movie.year;
+    return existingMovie.save();
   }
 }
 
-export async function get(id) {
-  const collection = await connect();
-  const doc = await collection.findOne({ id });
-  return doc;
-}
-
-async function update(movie) {
-  movie.id = parseInt(movie.id, 10);
-  const collection = await connect();
-  await collection.updateOne({ id: movie.id }, { $set: movie });
-  return movie;
+export function get(id) {
+  return Movie.findOne({ id });
 }
 
 export async function remove(id) {
-  const collection = await connect();
-  return collection.deleteOne({ id });
+  const movie = await get(id);
+  return movie.deleteOne();
 }
